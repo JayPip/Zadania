ec39d765a5e7dc366c6493def4150ffb54bf3b7e
JayPip <jakub.pipien@gmail.com>
2021-05-26 14:18:53 +0200

    commit#2

---
+++ zestaw6/zad1.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<c.math>
+using namespace std;
+
+int main(){
+
+int number;
+
+
+
+cin>>number;
+if(number>20)return -1;
+
+
+
+int A[number];
+int *p;
+p=A;
+
+for(int i =0; i< number; i++){
+*p=pow(2,i);
+p++;
+}
+
+
+
+
+return 0;
+}
---
+++ zestaw6/zad3.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int number;
+
+
+
+cin>>number;
+if(number>20)return -1;
+
+
+
+int A[number];
+int *p;
+p=A;
+
+for(int i =0; i< number; i++){
+cin>>*p;
+if(*p<0)*p=-*p;
+p++;
+}
+
+
+
+
+return 0;
+}
